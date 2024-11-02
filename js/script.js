f07daad53b96fb13c3da63dd72885c9ca79473c1
nandisimo04 <nenadsites@gmail.com>
2024-11-02 21:39:27 +0100

    asdfadfsfds

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 4, 2024 12:00:00").getTime();
+var countDownDate = new Date("Dec 4, 2024 23:59:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -11,12 +11,11 @@ var x = setInterval(function() {
   var distance = countDownDate - now;
     
   // Time calculations for days, hours, minutes and seconds
-  var weeks = Math.floor(distance / (1000 * 60 * 60 * 24 * 7))
+  var months = Math.floor(distance / (1000 * 60 * 60 * 24 * 30))
+  var weeks = Math.floor((distance % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24 * 7))
   var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24));
 
-  document.getElementById("demo").innerHTML = weeks + "w " + days + "d " 
-  
-    
+  document.getElementById("demo").innerHTML = months + "m " + weeks + "w " + days + "d " 
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
