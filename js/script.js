528be66578875133942b231a1f23e02a8a0615d1
nandisimo04 <nenadsites@gmail.com>
2024-11-02 21:17:43 +0100

    scascas

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2030 15:37:25").getTime();
+var countDownDate = new Date("Dec 4, 2024 12:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -11,14 +11,11 @@ var x = setInterval(function() {
   var distance = countDownDate - now;
     
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  var weeks = Math.floor(distance / (1000 * 60 * 60 * 24 * 7))
+  var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24));
+
+  document.getElementById("demo").innerHTML = weeks + "w " + days + "d " 
+  
     
   // If the count down is over, write some text 
   if (distance < 0) {
